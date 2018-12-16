931635e79d3f43cda09a7cebfe734635806ffbce
Angela Su <angelajsu@gmail.com>
2018-12-16 15:04:01 -0500

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                invert <<- NULL
+        }
+        get <- function() x
+        setinvert <- function(invert) i <<- invert
+        getinvert <- function() i
+        list(set = set, get = get,
+             setinvert = setinvert,
+             getinvert = getinvert)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        i <- x$getinvert() #attempts to retrieve mean from makeVector() object
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get() #gets the vector for the input object
+        i <- solve(data, ...) #calculates the mean, only place where it is executed
+        x$setinvert(i) #set the mean in the input object
+        i #prints mean object
 }
